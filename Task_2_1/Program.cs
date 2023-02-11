31201d7944e9043c49f4b0fcedc9c08b6d0f0364
Jon Miller <jordjik.don@gmail.com>
2023-02-11 19:22:33 +0300

    Task_2_1

--- Task_2_1/Program.cs
+++ Task_2_1/Program.cs
@@ -19,9 +19,34 @@ namespace Task_2_1
         {
             
             User user = new User();
+
+            //вывод время созданания обьекта user 
             Console.WriteLine(user.date.ToString());
 
+            //Вывод фамилии на экран
+            Console.WriteLine(user.Family);
 
+            //присвоение логина пользователя
+            Console.WriteLine("Введите логин пользователя...");
+            user.Login = Convert.ToString(Console.ReadLine());
+
+            //вводи имени 
+            Console.WriteLine("Введите имя пользователя...");
+            user.Name = Convert.ToString(Console.ReadLine());
+
+            //ввод фамилии
+            Console.WriteLine("Введие фамилию пользователя...");
+            user.Family = Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine($"Дата создания анкеты:{user.date.ToString()}");
+            Console.WriteLine($"Логин:{user.Login},Имя:{user.Name},Фамилия:{user.Family}");
+
+            Console.WriteLine(new string('-',30));
+
+            User user2 = new User("djd", "dfdf", "edde", 34);
+            Console.WriteLine(user2.date.ToString(),user2.Name,user2.Family,user2.Login);
+
+            Console.ReadKey();
         }
     }
 }
